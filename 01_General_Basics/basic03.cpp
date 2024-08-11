17b5f9db6efb6cb4d3bc1199e056c76c177c70a5
UNacc Shubh <154262783+KratosHere@users.noreply.github.com>
2024-08-11 19:31:15 +0000

    line input

---
+++ 01_General_Basics/basic03.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    cout << "Who is Shubhashish Chakraborty's Future Wife: ";
+
+    string wifeName;
+    getline(cin , wifeName);
+
+    cout << wifeName + " is Shubhashish Chakraborty's Future wife" << endl;
+
+    return 0;
+}
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+
+//     // Taking input and displaying the whole line!!
+
+//     string UserName;
+
+//     cout << "Enter Your Name:";
+
+//     getline(cin , UserName);
+
+//     cout << "hello " + UserName << endl;
+
+//     return 0;
+// }
