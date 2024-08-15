cfd133ef0ca36330ed913970fa6e5883ec082569
UNacc Shubh <154262783+KratosHere@users.noreply.github.com>
2024-08-15 06:52:32 +0000

    started with 1D array!

---
+++ 03_Arrays/arr01.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    // Similar Data Array! , Every Data you enter should be of similar data type!
+
+    int arr[5]; // arr[n] => this will create an array of size => n (5)
+
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+
+    // It is manipulative!
+
+    arr[0] += 10;
+
+    // cout << arr[0] << endl;
+
+
+
+
+
+
+    return 0;
+}
